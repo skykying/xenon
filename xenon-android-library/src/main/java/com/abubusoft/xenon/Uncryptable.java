843af4ede6c6eb12c58c41e7822ddaf49391698b
xcesco <abubusoft@gmail.com>
2017-12-13 22:03:10 +0100

    first commit

--- xenon-android-library/src/main/java/com/abubusoft/xenon/Uncryptable.java
+++ xenon-android-library/src/main/java/com/abubusoft/xenon/Uncryptable.java
@@ -3,11 +3,18 @@
  */
 package com.abubusoft.xenon;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Interfaccia che serve a marcare come non cryptabili (con proguard)
  * @author Francesco Benincasa
  *
  */
-public interface Uncryptable {
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value= ElementType.TYPE)
+public @interface Uncryptable {
 
 }
